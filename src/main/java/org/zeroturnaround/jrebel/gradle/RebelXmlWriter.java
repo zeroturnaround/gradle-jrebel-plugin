65b131bd75b921021ed00b96f664bce4c4a0e78d
sander <sander.sonajalg@gmail.com>
2013-07-04 18:24:30 +0300

    split xmlWriter to smaller methods

--- src/main/java/org/zeroturnaround/jrebel/gradle/RebelXmlWriter.java
+++ src/main/java/org/zeroturnaround/jrebel/gradle/RebelXmlWriter.java
@@ -18,7 +18,7 @@ import org.zeroturnaround.jrebel.gradle.model.RebelMainModel;
  *
  * XXX - jesus christ, there must be tens of better ways to generate xml! why....????? [sander]
  * 
- * @author Igor Bljahhin
+ * @author Igor Bljahhin (or maybe just a copy-paste from the Maven plugin?)
  */
 public class RebelXmlWriter {
   
@@ -29,74 +29,21 @@ public class RebelXmlWriter {
     Writer writer = new StringWriter();
 
     try {
-      writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-      writer.write("<application xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.zeroturnaround.com\" xsi:schemaLocation=\"http://www.zeroturnaround.com http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd\">\n");
-      writer.write("\n");
-      writer.write("\t<classpath");
+      writeHeader(writer);
       
-      if (model.getFallbackClasspath() != null) {
-        writer.write(" fallback=\"" + model.getFallbackClasspath() + "\"");
-      }
-      writer.write(">\n");
+      writeClasspath(model, writer);
   
-      for (RebelClasspathResource r : model.getClasspathDirs()) {
-        writer.write("\t\t<dir name=\"" + escapeXml(r.getDirectory())
-            + "\">\n");
-        writeExcludeInclude(writer, r);
-        writer.write("\t\t</dir>\n");
-      }
+      writeWar(model, writer);
+      
+      writeWeb(model, writer);
   
-      for (RebelClasspathResource r : model.getClasspathJars()) {
-        writer.write("\t\t<jar name=\"" + escapeXml(r.getJar()) + "\">\n");
-        writeExcludeInclude(writer, r);
-        writer.write("\t\t</jar>\n");
-      }
-  
-      for (RebelClasspathResource r : model.getClasspathJarsets()) {
-        writer.write("\t\t<jarset dir=\"" + escapeXml(r.getJarset())
-            + "\">\n");
-        writeExcludeInclude(writer, r);
-        writer.write("\t\t</jarset>\n");
-      }
-  
-      for (RebelClasspathResource r : model.getClasspathDirsets()) {
-        writer.write("\t\t<dirset dir=\"" + escapeXml(r.getDirset())
-            + "\">\n");
-        writeExcludeInclude(writer, r);
-        writer.write("\t\t</dirset>\n");
-      }
-  
-      writer.write("\t</classpath>\n");
-      writer.write("\n");
-  
-      RebelWar war = model.getWar();
-      if (war != null && war.getPath() != null) {
-        writer.write("\t<war dir=\"" + escapeXml(war.getPath()) + "\"/>\n");
-        writer.write("\n");
-      }
-  
-      if (model.getWebResources().size() > 0) {
-        writer.write("\t<web>\n");
-        for (RebelWebResource r : model.getWebResources()) {
-          writer.write("\t\t<link target=\"" + escapeXml(r.getTarget())
-              + "\">\n");
-          writer.write("\t\t\t<dir name=\"" + escapeXml(r.getDirectory())
-              + "\">\n");
-          writeExcludeInclude(writer, r);
-          writer.write("\t\t\t</dir>\n");
-          writer.write("\t\t</link>\n");
-        }
-        writer.write("\t</web>\n");
-        writer.write("\n");
-      }
-  
-      writer.write("</application>\n");
+      writeFooter(writer);
+      
       writer.flush();
-      
       return writer.toString();
     }
     // can't see it happening
-    catch (IOException _ignore) {
+    catch (IOException e) {
       return null;
     }
     finally {
@@ -105,21 +52,112 @@ public class RebelXmlWriter {
         try {
           writer.close();
         }
-        catch (IOException _ignore) {
+        catch (IOException e) {
         }
       }
     } 
   }
 
-  private void writeExcludeInclude(Writer writer, RebelClasspathResource r)
-      throws IOException
-  {
-    writeExcludeInclude(writer, r, 3);
+  private void writeHeader(Writer writer) throws IOException {
+    writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+    writer.write("<application xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.zeroturnaround.com\" xsi:schemaLocation=\"http://www.zeroturnaround.com http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd\">\n");
+    writer.write("\n");
+  }
+  
+  /**
+   * Write the <classpath> element.
+   */
+  private void writeClasspath(RebelMainModel model, Writer writer) throws IOException {
+    writer.write("\t<classpath");
+    
+    if (model.getFallbackClasspath() != null) {
+      writer.write(" fallback=\"" + model.getFallbackClasspath() + "\"");
+    }
+    writer.write(">\n");
+ 
+    // Classpath dirs
+    for (RebelClasspathResource resource : model.getClasspathDirs()) {
+      writer.write("\t\t<dir name=\"" + escapeXml(resource.getDirectory())
+          + "\">\n");
+      writeExcludeInclude(writer, resource);
+      writer.write("\t\t</dir>\n");
+    }
+ 
+    // Classpath jars
+    for (RebelClasspathResource resource : model.getClasspathJars()) {
+      writer.write("\t\t<jar name=\"" + escapeXml(resource.getJar()) + "\">\n");
+      writeExcludeInclude(writer, resource);
+      writer.write("\t\t</jar>\n");
+    }
+ 
+    // Classpath jarsets
+    for (RebelClasspathResource resource : model.getClasspathJarsets()) {
+      writer.write("\t\t<jarset dir=\"" + escapeXml(resource.getJarset())
+          + "\">\n");
+      writeExcludeInclude(writer, resource);
+      writer.write("\t\t</jarset>\n");
+    }
+ 
+    // Classpath dirsets
+    for (RebelClasspathResource resource : model.getClasspathDirsets()) {
+      writer.write("\t\t<dirset dir=\"" + escapeXml(resource.getDirset())
+          + "\">\n");
+      writeExcludeInclude(writer, resource);
+      writer.write("\t\t</dirset>\n");
+    }
+ 
+    writer.write("\t</classpath>\n");
+    writer.write("\n");
   }
 
-  private void writeExcludeInclude(Writer writer, RebelResource r, int indent)
-      throws IOException
-  {
+  /**
+   * Write the <web> element.
+   */
+  private void writeWeb(RebelMainModel model, Writer writer) throws IOException {
+    // web resources
+    if (model.getWebResources().size() > 0) {
+      writer.write("\t<web>\n");
+      for (RebelWebResource r : model.getWebResources()) {
+        writer.write("\t\t<link target=\"" + escapeXml(r.getTarget())
+            + "\">\n");
+        writer.write("\t\t\t<dir name=\"" + escapeXml(r.getDirectory())
+            + "\">\n");
+        writeExcludeInclude(writer, r);
+        writer.write("\t\t\t</dir>\n");
+        writer.write("\t\t</link>\n");
+      }
+      writer.write("\t</web>\n");
+      writer.write("\n");
+    }
+  }
+
+  /**
+   * Write the <war> element.
+   */
+  private void writeWar(RebelMainModel model, Writer writer) throws IOException {
+    RebelWar war = model.getWar();
+    if (war != null && war.getPath() != null) {
+      writer.write("\t<war dir=\"" + escapeXml(war.getPath()) + "\"/>\n");
+      writer.write("\n");
+    }
+  }
+
+  private void writeFooter(Writer writer) throws IOException {
+    writer.write("</application>\n");
+  }
+
+  private void writeExcludeInclude(Writer writer, RebelClasspathResource r) throws IOException {
+    writeExcludeIncludeImpl(writer, r, 3);
+  }
+
+  private void writeExcludeInclude(Writer writer, RebelWebResource r) throws IOException {
+    writeExcludeIncludeImpl(writer, r, 4);
+  }
+
+  /**
+   * Write <exclude> and <include> elements. This code is shared between writing multiple resources.
+   */
+  private void writeExcludeIncludeImpl(Writer writer, RebelResource r, int indent) throws IOException {
     String indention = repeat("\t", indent);
 
     if (r.getExcludes() != null) {
@@ -137,10 +175,4 @@ public class RebelXmlWriter {
     }
   }
 
-  private void writeExcludeInclude(Writer writer, RebelWebResource r)
-      throws IOException
-  {
-    writeExcludeInclude(writer, r, 4);
-  }
-  
 }
