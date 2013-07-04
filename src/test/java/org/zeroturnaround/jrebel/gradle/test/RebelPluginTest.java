b493ddaf04b23bbe375be1869eb74a0f843ce877
sander <sander.sonajalg@gmail.com>
2013-07-04 20:26:28 +0300

    turning the groovy test into java

---
+++ src/test/java/org/zeroturnaround/jrebel/gradle/test/RebelPluginTest.java
@@ -0,0 +1,137 @@
+/**
+ *  Copyright (C) 2012 ZeroTurnaround <support@zeroturnaround.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.zeroturnaround.jrebel.gradle.test;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.plugins.GroovyPlugin;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.WarPlugin;
+import org.gradle.api.plugins.jetty.JettyPlugin;
+import org.gradle.api.tasks.TaskExecutionException;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.Test;
+import org.zeroturnaround.jrebel.gradle.RebelGenerateTask;
+import org.zeroturnaround.jrebel.gradle.RebelPlugin;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * General tests for plugins integration with Gradle lifecycles, etc.
+ * 
+ * @author Igor Bljahhin, Sander Sonajalg
+ */
+public class RebelPluginTest {
+
+  /**
+   * Test that the plugin adds a dummy task to the project when no JavaPlugin is applied
+   * @throws TaskExecutionException 
+   */
+  @Test(expected = IllegalStateException.class)
+  public void testAddsDummyTaskWhenJavaPluginNotApplied() throws Throwable {
+    Project project = ProjectBuilder.builder().build();
+    project.getProject().getPlugins().apply(RebelPlugin.class);
+
+    //def task = project.getTasks().generateRebel;
+    Task task = project.getTasks().getByName("generateRebel");
+    assertTrue(task instanceof DefaultTask);
+    try {
+      ((DefaultTask) task).execute();
+    }
+    catch (TaskExecutionException e) {
+      throw e.getCause();
+    }
+  }
+
+  /**
+   * Test that the plugin adds RebelGenerateTask to project when JavaPlugin is already applied
+   */
+  @Test
+  public void testAddsRebelTaskWhenJavaPluginApplied() {
+    Project project = ProjectBuilder.builder().build();
+    project.getProject().getPlugins().apply(JavaPlugin.class);
+    project.getProject().getPlugins().apply(RebelPlugin.class);
+
+    Task task = project.getTasks().getByName("generateRebel");
+    assertTrue(task instanceof RebelGenerateTask);
+    
+    RebelGenerateTask rebelTask = (RebelGenerateTask) task;
+    assertTrue(rebelTask.getPackaging().equals("jar"));
+    
+    // XXX restore
+    // this used to work when the test was in groovy. but what did it test and how to restore in java?
+    //assertTrue(project.getTasks().getClasses() in task.dependsOn);
+  }
+
+  /**
+   * Test that the plugin adds rebel task to project after GroovyPlugin is applied
+   */
+  @Test
+  public void testAddsRebelTaskAfterGroovyPluginApplied() {
+    Project project = ProjectBuilder.builder().build();
+    project.getProject().getPlugins().apply(RebelPlugin.class);
+    project.getProject().getPlugins().apply(GroovyPlugin.class);
+
+    Task task = project.getTasks().getByName("generateRebel");
+    assertTrue(task instanceof RebelGenerateTask);
+    
+    RebelGenerateTask rebelTask = (RebelGenerateTask) task;
+    assertTrue(rebelTask.getPackaging().equals("jar"));
+    
+    // XXX restore
+    // assertTrue(project.tasks.classes in task.dependsOn);
+  }
+
+  /**
+   * Test that the plugin uses war packaging mode when WarPlugin is already applied
+   */
+  @Test
+  public void testUsesWarPackagingWithWarPlugin() {
+    Project project = ProjectBuilder.builder().build();
+    project.getProject().getPlugins().apply(WarPlugin.class);
+    project.getProject().getPlugins().apply(RebelPlugin.class);
+
+    Task task = project.getTasks().getByName("generateRebel");
+    assertTrue(task instanceof RebelGenerateTask);
+    
+    RebelGenerateTask rebelTask = (RebelGenerateTask) task;
+    assertTrue(rebelTask.getPackaging().equals("war"));
+    
+    // XXX restore
+    //assertTrue(project.tasks.classes in task.dependsOn);
+  }
+
+  
+  /**
+   * Test that the plugin uses war packaging mode after JettyPlugin gets applied
+   */
+  @Test
+  public void testUsesWarPackagingWithJettyPlugin() {
+    Project project = ProjectBuilder.builder().build();
+    project.getProject().getPlugins().apply(RebelPlugin.class);
+    project.getProject().getPlugins().apply(JettyPlugin.class);
+
+    Task task = project.getTasks().getByName("generateRebel");
+    assertTrue(task instanceof RebelGenerateTask);
+    
+    RebelGenerateTask rebelTask = (RebelGenerateTask) task;
+    assertTrue(rebelTask.getPackaging().equals("war"));
+    
+    // XXX restore
+    //assertTrue(project.tasks.classes in task.dependsOn);
+  }
+}
